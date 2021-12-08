da745bd86a63cbf12b5cb410761fb31cb96c0c26
dev1 <dev1provid@gmail.com>
2021-12-09 00:54:18 +0300

    added some user queries, mutations

--- server/users/seeFollowers/seeFollowers.resolvers.js
+++ server/users/seeFollowers/seeFollowers.resolvers.js
@@ -4,6 +4,7 @@ export default {
   Query: {
     seeFollowers: async (_, {username, page}) => {
       const ok = await prisma.user.findUnique({where: { username }, select: { id: true }});
+      if (!ok) return { ok: false, error: 'User not found' }
       const followers = await prisma.user.findUnique({where: {username}}).followers({ take: 5, skip: (page - 1) * 5 });
       const totalFollowers = await prisma.user.count({ where: { following: { some: { username } } }})
       return {
